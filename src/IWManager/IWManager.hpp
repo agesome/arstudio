7cc35042c77ca9f3a9e9cacf99da54b3b4269dbd
Evgeny Grablyk <evgeny.grablyk@gmail.com>
2013-06-14 14:58:09 +0300

    IWManager: added camera following capability
    When enabled, this ensures that viewport appears as if looking out
    of a camera, positioned and oriented according to  last processed
    Camera object.

--- src/IWManager/IWManager.hpp
+++ src/IWManager/IWManager.hpp
@@ -1,8 +1,11 @@
 #ifndef IWMANAGER_HPP
 #define IWMANAGER_HPP
 
+#include <cmath>
+
 #include <QQuickPaintedItem>
 #include <QQmlEngine>
+#include <QKeyEvent>
 
 #ifndef QT_NO_DEBUG
 #include <QDateTime>
@@ -12,6 +15,9 @@
 #include <QQuickMesh>
 #include <QQuickEffect>
 #include <QGraphicsRotation3D>
+#include <QGLCamera>
+#include <QPlane3D>
+#include <QRay3D>
 
 #include <line.h>
 #include <spheremesh.h>
@@ -39,27 +45,57 @@ class IWManager : public QObject
   Q_OBJECT
   Q_PROPERTY (arstudio::Scenegraph * scenegraph READ scenegraph)
   Q_PROPERTY (QQuickPaintedItem * viewport READ viewport WRITE set_viewport)
+  Q_PROPERTY (QGLCamera * camera READ camera WRITE set_camera)
+  Q_PROPERTY (bool camera_view READ camera_view WRITE set_camera_view)
+  Q_PROPERTY (QVector3D camera_view_position READ camera_view_position
+              NOTIFY camera_view_position_changed)
+  Q_PROPERTY (qreal camera_view_distance READ camera_view_distance
+              WRITE set_camera_view_distance)
 public:
   IWManager (QObject * parent = nullptr);
 
   QQuickPaintedItem * viewport (void);
   void set_viewport (QQuickPaintedItem * viewport);
+  QGLCamera * camera (void);
+  void set_camera (QGLCamera *);
+  bool camera_view (void);
+  void set_camera_view (bool);
+
+  QVector3D camera_view_position (void);
+  qreal camera_view_distance (void);
+  void set_camera_view_distance (qreal);
+
   arstudio::Scenegraph * scenegraph (void);
 private:
+  enum Axis { X, Y, Z };
+
   void add_camera (Camera::ptr);
   void add_camera_path (Sequence *);
+  inline void add_line (const QList<QVariant> &);
+  inline void add_line (const QList<QVector3D> &);
+  inline void add_sphere (const QVector3D &);
+  void look_from_camera (Camera::ptr);
+
+  inline void reset_camera (const Axis axis = Z);
 
   QQuickPaintedItem * m_viewport;
-  CylinderMesh      * m_camera_mesh;
+  QGLCamera         * m_camera;
+  CylinderMesh      * m_cylinder_mesh;
   QQuickEffect      * m_camera_effect;
-  SphereMesh        * m_path_mesh;
+  SphereMesh        * m_sphere_mesh;
   QQuickEffect      * m_path_effect;
   QQuickEffect      * m_line_effect;
   Scenegraph::ptr     m_scenegraph;
   int                 m_current_frame;
+  bool                m_camera_view;
+  qreal               m_camera_view_distance;
+  QVector3D           m_camera_view_position;
 public slots:
   void paint_frame (int);
   void repaint_frame (void);
+  void viewport_keypress (const Qt::Key);
+signals:
+  void camera_view_position_changed (void);
 };
 }
 
