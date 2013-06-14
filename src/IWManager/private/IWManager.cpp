25bb419f843f0d141dab002c9386d48c9e346997
Evgeny Grablyk <evgeny.grablyk@gmail.com>
2013-06-14 14:41:20 +0300

    IWManager: Added camera following capability.
    Setting camera_view property makes IWManager calculate the position
    where the last rendered Camera object was "looking" and expose it via
    camera_view_position. At the same time, the Viewport camera is moved
    to look at that position, as if we were looking through the camera.

--- src/IWManager/private/IWManager.cpp
+++ src/IWManager/private/IWManager.cpp
@@ -4,13 +4,16 @@ namespace arstudio {
 IWManager::IWManager (QObject * parent)
   : QObject (parent),
   m_viewport (nullptr),
-  m_camera_mesh (nullptr),
+  m_camera (nullptr),
+  m_cylinder_mesh (nullptr),
   m_camera_effect (nullptr),
-  m_path_mesh (nullptr),
+  m_sphere_mesh (nullptr),
   m_path_effect (nullptr),
   m_line_effect (nullptr),
   m_scenegraph (Scenegraph::make ()),
-  m_current_frame (1)
+  m_current_frame (1),
+  m_camera_view (false),
+  m_camera_view_distance (1.5)
 {
   ScenegraphAggregator * sa = ScenegraphAggregator::instance ();
 
@@ -35,26 +38,74 @@ IWManager::set_viewport (QQuickPaintedItem * viewport)
   if (m_camera_effect)
     {
       delete m_camera_effect;
-      delete m_camera_mesh;
+      delete m_cylinder_mesh;
       delete m_path_effect;
-      delete m_path_mesh;
+      delete m_sphere_mesh;
       delete m_line_effect;
     }
 
   m_camera_effect = new QQuickEffect (viewport);
-  m_camera_mesh   = new CylinderMesh (viewport);
+  m_cylinder_mesh = new CylinderMesh (viewport);
   m_path_effect   = new QQuickEffect (viewport);
-  m_path_mesh     = new SphereMesh (viewport);
+  m_sphere_mesh   = new SphereMesh (viewport);
   m_line_effect   = new QQuickEffect (viewport);
 
   m_camera_effect->setColor (QColor::fromRgb (250, 0, 0));
-  m_camera_mesh->setRadius (0.008);
-  m_camera_mesh->setLength (0.12);
+  m_cylinder_mesh->setRadius (0.008);
+  m_cylinder_mesh->setLength (0.12);
 
   m_path_effect->setColor (QColor::fromRgb (0, 0, 250));
-  m_path_mesh->setRadius (0.008);
+  m_sphere_mesh->setRadius (0.008);
 
   m_line_effect->setColor (QColor::fromRgb (0, 250, 0));
+}
+
+QGLCamera *
+IWManager::camera ()
+{
+  return m_camera;
+}
+
+void
+IWManager::set_camera (QGLCamera * camera)
+{
+  m_camera = camera;
+}
+
+bool
+IWManager::camera_view ()
+{
+  return m_camera_view;
+}
+
+void
+IWManager::set_camera_view (bool v)
+{
+  m_camera_view = v;
+  if (!v)
+    reset_camera ();
+  else
+    // set near plane for close object viewing
+    m_camera->setNearPlane (1.0);
+  repaint_frame ();
+}
+
+QVector3D
+IWManager::camera_view_position ()
+{
+  return m_camera_view_position;
+}
+
+qreal
+IWManager::camera_view_distance ()
+{
+  return m_camera_view_distance;
+}
+
+void
+IWManager::set_camera_view_distance (qreal d)
+{
+  m_camera_view_distance = d;
 }
 
 Scenegraph *
@@ -87,7 +138,7 @@ IWManager::add_camera (Camera::ptr c)
   p.append (&p, rz);
 
   i->setEffect (m_camera_effect);
-  i->setMesh (m_camera_mesh);
+  i->setMesh (m_cylinder_mesh);
   i->setPosition (c->position ());
   i->setParentItem (m_viewport);
 }
@@ -95,41 +146,129 @@ IWManager::add_camera (Camera::ptr c)
 void
 IWManager::add_camera_path (Sequence * s)
 {
-  QList<QVariant> points;
-  Line          * l = new Line (m_viewport);
+  QList<QVector3D> points;
+  QVector3D        p;
 
   for (auto ip : s->items ())
     {
       Item::ptr   i = ip.second;
       Camera::ptr c = i.dynamicCast<Camera> ();
-      points << c->position ().x ();
-      points << c->position ().y ();
-      points << c->position ().z ();
 
-      QQuickItem3D * p = new QQuickItem3D (m_viewport);
-      p->setMesh (m_path_mesh);
-      p->setEffect (m_path_effect);
-      p->setPosition (c->position ());
-      p->setParentItem (m_viewport);
+      p = c->position ();
+      points << p;
+      add_sphere (p);
     }
 
-  l->setWidth (2.0);
+  add_line (points);
+}
+
+void
+IWManager::add_line (const QList<QVariant> & vertices)
+{
+  Line * l = new Line (m_viewport);
+
   l->setEffect (m_line_effect);
-  l->setVertices (points);
+  l->setWidth (2.0);
+  l->setVertices (vertices);
   l->setParentItem (m_viewport);
+}
+
+void
+IWManager::add_line (const QList<QVector3D> & points)
+{
+  QList<QVariant> vertices;
+  for (auto v : points)
+    {
+      vertices << v.x ();
+      vertices << v.y ();
+      vertices << v.z ();
+    }
+  add_line (vertices);
+}
+
+void
+IWManager::add_sphere (const QVector3D & location)
+{
+  QQuickItem3D * s = new QQuickItem3D (m_viewport);
+
+  s->setMesh (m_sphere_mesh);
+  s->setEffect (m_path_effect);
+  s->setPosition (location);
+  s->setParentItem (m_viewport);
+}
+
+void
+IWManager::look_from_camera (Camera::ptr c)
+{
+  const float x = (c->rotation ().x () / 180) * M_PI;
+  const float y = (c->rotation ().y () / 180) * M_PI;
+
+  const float c1 = std::cos (x);
+  const float s1 = std::sin (x);
+  const float c2 = std::cos (y);
+  const float s2 = std::sin (y);
+
+  // direction vector, aligned with camera
+  QVector3D camera_direction (s2, -c2 * s1, c2 * c1);
+  QRay3D    camera_direction_ray (c->position (), camera_direction);
+
+  m_camera_view_position = camera_direction_ray.point (m_camera_view_distance);
+  camera_view_position_changed ();
+
+  QPlane3D  ground (QVector3D (0, 0, 0), QVector3D (0, 1, 0));
+  qreal     it = ground.intersection (camera_direction_ray);
+  QVector3D ip = camera_direction_ray.point (it);
+  ip.setY (0);
+  ip -= camera_direction_ray.point (it + 1);
+  QVector3D ipp (-ip.z (), 0, ip.x ());
+  QRay3D    ipp_ray (camera_direction_ray.point (it), ipp);
+
+  QPlane3D view_plane (c->position (),
+                       camera_view_position (),
+                       ipp_ray.point (1.0));
+
+  m_camera->setCenter (m_camera_view_position);
+  m_camera->setEye (c->position ());
+  m_camera->setUpVector (view_plane.normal ());
+}
+
+void
+IWManager::reset_camera (const IWManager::Axis axis)
+{
+  switch (axis)
+    {
+    case X:
+      m_camera->setEye (QVector3D (10, 0, 0));
+      m_camera->setCenter (QVector3D (0, 0, 0));
+      m_camera->setUpVector (QVector3D (0, 1, 0));
+      break;
+
+    case Y:
+      m_camera->setEye (QVector3D (0, 10, 0));
+      m_camera->setCenter (QVector3D (0, 0, 0));
+      m_camera->setUpVector (QVector3D (1, 0, 0));
+
+    case Z:
+      m_camera->setEye (QVector3D (0, 0, 10));
+      m_camera->setCenter (QVector3D (0, 0, 0));
+      m_camera->setUpVector (QVector3D (0, 1, 0));
+    }
+  m_camera->setNearPlane (5.0);
 }
 
 void
 IWManager::paint_frame (int frame)
 {
   Q_ASSERT (m_viewport);
+  Camera::ptr eye_camera;
+
 #ifndef QT_NO_DEBUG
   qint64 time = QDateTime::currentMSecsSinceEpoch ();
 #endif
   QList<QQuickItem *> ci = m_viewport->childItems ();
   for (QQuickItem * i : ci)
     {
-      if (i->objectName () == "vp_axis")
+      if (i->objectName ().contains ("vp_ignore"))
         continue;
       delete i;
     }
@@ -142,7 +281,8 @@ IWManager::paint_frame (int frame)
       switch (s->type ())
         {
         case Sequence::CAMERA:
-          add_camera (p.dynamicCast<Camera> ());
+          eye_camera = p.dynamicCast<Camera> ();
+          add_camera (eye_camera);
           add_camera_path (s);
           break;
 
@@ -150,6 +290,10 @@ IWManager::paint_frame (int frame)
           return;
         }
     }
+
+  if (m_camera_view && m_camera && eye_camera)
+    look_from_camera (eye_camera);
+
   m_viewport->update ();
   m_current_frame = frame;
 #ifndef QT_NO_DEBUG
@@ -162,5 +306,25 @@ void
 IWManager::repaint_frame (void)
 {
   paint_frame (m_current_frame);
+}
+
+void
+IWManager::viewport_keypress (const QKeyEvent e)
+{
+  switch (e.key ())
+    {
+    case Qt::Key_X:
+      reset_camera (X);
+      break;
+
+    case Qt::Key_Y:
+      reset_camera (Y);
+      break;
+
+    case Qt::Key_Z:
+    case Qt::Key_Space:
+      reset_camera (Z);
+      break;
+    }
 }
 }
