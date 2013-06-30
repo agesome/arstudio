5b617e390d9548962a5f09c9c43cec27a1c884ea
Evgeny Grablyk <evgeny.grablyk@gmail.com>
2013-06-30 13:48:02 +0300

    Simplified CustomModel

--- src/CustomModel/CustomModel.hpp
+++ src/CustomModel/CustomModel.hpp
@@ -15,34 +15,18 @@ class CustomModel : public QQuickItem3D
 {
   Q_OBJECT
   Q_PROPERTY (QVector3D rotation READ rotation WRITE set_rotation)
-  Q_PROPERTY (qreal scale READ scale WRITE set_scale)
-  Q_PROPERTY (QQuickEffect * effect READ effect WRITE set_effect)
-  Q_PROPERTY (QString filename READ filename NOTIFY filename_changed)
 public:
   CustomModel (QObject * parent = nullptr);
 
   QVector3D rotation ();
   void set_rotation (const QVector3D &);
-  QQuickEffect * effect ();
-  void set_effect (QQuickEffect *);
 
   void set_source (const QUrl &);
-
-  QString filename ();
-
-  qreal scale ();
-  void set_scale (qreal);
 private:
   QVector3D             m_rotation;
-  qreal                 m_scale;
-  QQuickEffect        * m_effect;
-  QQuickMesh          * m_mesh;
   QGraphicsRotation3D * m_rotation_x;
   QGraphicsRotation3D * m_rotation_y;
   QGraphicsRotation3D * m_rotation_z;
-  QString               m_filename;
-signals:
-  void filename_changed ();
 };
 }
 
--- src/CustomModel/private/CustomModel.cpp
+++ src/CustomModel/private/CustomModel.cpp
@@ -3,13 +3,9 @@
 namespace arstudio {
 CustomModel::CustomModel (QObject * parent)
   : QQuickItem3D (parent),
-  m_scale (0.1),
-  m_effect (nullptr),
-  m_mesh (new QQuickMesh (this)),
   m_rotation_x (new QGraphicsRotation3D (this)),
   m_rotation_y (new QGraphicsRotation3D (this)),
-  m_rotation_z (new QGraphicsRotation3D (this)),
-  m_filename ("invalid")
+  m_rotation_z (new QGraphicsRotation3D (this))
 {
   m_rotation_x->setAxis (QVector3D (1, 0, 0));
   m_rotation_y->setAxis (QVector3D (0, 1, 0));
@@ -20,9 +16,8 @@ CustomModel::CustomModel (QObject * parent)
   p.append (&p, m_rotation_y);
   p.append (&p, m_rotation_z);
 
-  setMesh (m_mesh);
   setObjectName ("vp_ignore");
-  setScale (m_scale);
+  setScale (0.1);
 }
 
 QVector3D
@@ -40,19 +35,6 @@ CustomModel::set_rotation (const QVector3D & r)
   m_rotation_z->setAngle (r.z ());
 }
 
-QQuickEffect *
-CustomModel::effect ()
-{
-  return m_effect;
-}
-
-void
-CustomModel::set_effect (QQuickEffect * e)
-{
-  m_effect = e;
-  e->setParent (this);
-}
-
 void
 CustomModel::set_source (const QUrl & s)
 {
@@ -64,27 +46,14 @@ CustomModel::set_source (const QUrl & s)
       return;
     }
 
-  m_mesh->setSource (s);
-  m_filename = i.fileName ();
-  filename_changed ();
-}
+  if (mesh ())
+    {
+      auto m = mesh ();
+      setMesh (nullptr);
+      delete m;
+    }
 
-QString
-CustomModel::filename ()
-{
-  return m_filename;
-}
-
-qreal
-CustomModel::scale ()
-{
-  return m_scale;
-}
-
-void
-CustomModel::set_scale (qreal s)
-{
-  m_scale = s;
-  setScale (s);
+  setMesh (new QQuickMesh (this));
+  mesh ()->setSource (s);
 }
 }
