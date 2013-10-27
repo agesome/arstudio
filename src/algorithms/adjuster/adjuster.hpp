2292e85d468d717458852e19860171f989896c1d
Evgeny Grablyk <evgeny.grablyk@gmail.com>
2013-10-27 23:36:30 +0200

    Fixed include path in adjuster

--- src/algorithms/adjuster/adjuster.hpp
+++ src/algorithms/adjuster/adjuster.hpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/features2d/features2d.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
 #include <Logger.hpp>
